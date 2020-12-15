c3a26dff7bdbebfa93610935940a14894cb369ee
Julian Rojas <julian.rojas87@gmail.com>
2020-12-15 23:36:57 +0100

    Dockerize

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM openjdk:16-slim-buster
+
+RUN apt-get update; apt-get install -y curl \
+    && curl -sL https://deb.nodesource.com/setup_14.x | bash - \
+    && apt-get install -y nodejs \
+    && curl -L https://www.npmjs.com/install.sh | sh
+#Creating a new directory for app files and setting path in the container
+RUN mkdir -p /usr/src/app
+#setting working directory in the container
+WORKDIR /usr/src/app
+#copying the package.json file(contains dependencies) from project source dir to container dir
+COPY package.json /usr/src/app
+# installing the dependencies into the container
+RUN npm install
+#copying the source code of Application into the container dir
+COPY . /usr/src/app
+#container exposed network port number
+EXPOSE 8080
+#command to run within the container
+ENTRYPOINT ["node", "bin/timeseries-server.js", "-c", "config.json"]
+#CMD ["bin/timeseries-server.js -c config.json"]
---
+++ bin/timeseries-server.js
@@ -0,0 +1,53 @@
+const CommunicationManager = require('../lib/CommunicationManager');
+const Configuration = require('../lib/Configuration');
+const SourceReader = require('../lib/SourceReader');
+const DataEventManager = require('../lib/DataEventManager');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+try {
+    // Read config file
+    let config = Configuration.getConfig(process.argv);
+    // Init Communication Manager module
+    let commManager = new CommunicationManager(config);
+
+    // Process data source
+    for (let i in config.sources) {
+
+        // Load multidimensional interfaces
+        loadInterfaceModules(config.sources[i], commManager);
+
+        let source = new SourceReader(config.sources[i], process.env.HOSTNAME + config.liveUriPath);
+        source.on('data', data => {
+            console.log(`${new Date().toISOString()} – data mapped from ${config.sources[i].name}`);
+            // Launch data event towards predefined interfaces through Data Event Manager module
+            DataEventManager.push(`data-${config.sources[i].name}`, data);
+        });
+    }
+
+    // TODO: Define a way to configure RDF input streams
+    // Listen for data on standard input
+    // let stdin = process.openStdin();
+    // stdin.on('data', chunk => writePerObservation(chunk));
+
+    // Launch Web server for polling interfaces
+    let app = commManager.app;
+    let http = commManager.http;
+    let ws = commManager.ws;
+    app.use(http.routes()).use(http.allowedMethods());
+    app.ws.use(ws.routes()).use(ws.allowedMethods());
+    app.listen(process.env.PORT);
+
+} catch (e) {
+    console.error(e);
+    process.exit(1);
+}
+
+function loadInterfaceModules(source, commManager) {
+    let int = Object.keys(source.interfaces);
+    for (let i in int) {
+        let Interface = require(process.cwd() + '/' + source.interfaces[int[i]]);
+        new Interface(source, commManager);
+    }
+}
